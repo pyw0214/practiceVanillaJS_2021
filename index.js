7904daabd61ddcd5a673673939f9d7a4aa1cf005
pyw0214 <yongwookp@gmail.com>
2021-02-06 08:25:18 +0900

    variablesUpdated_0206

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Im working. Im JS. Im beautiful. Im worth it');
+/* String
+
+const what = "Yong";
+console.log(what);
+
+*/
+
+/*Boolean (lowerCase)
+
+const what = true;
+*/
+
+//Number
+//const what = 4444;
+
+/*Float
+const what = 5.32;
+
+*/
