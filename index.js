a9cd78177a3269e8ef861a90f134a837439d84fe
pyw0214 <yongwookp@gmail.com>
2021-02-06 18:59:08 +0900

    event_conditionsUpdated0206

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const title = document.getElementById("title");
+const age = prompt("How old are you?");
 
-title.innerHTML = "Hi! From JS";
+if (age > 18 && age <= 21 ){
+    console.log("you can drink but what about not to?");
+} else if (age > 21 ) {
+    console.log("Go ahead");
+ } else {
+        console.log("Too young");
+    }
