3ede17256b2b27876b78beb1c3a9988343a59806
pyw0214 <yongwookp@gmail.com>
2021-02-08 20:39:24 +0900

    add_clock_userInput_updated_0208

---
+++ greeting.js
@@ -0,0 +1,28 @@
+const form = document.querySelector(".js-form"),
+input = form.getElementsByTagName("input"),
+greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
+
+function paintGreeting(text){
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`
+}
+
+
+function loadName(){
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser === null){
+        //she is not
+    } else {
+        paintGreeting(currentUser);
+    }
+}
+
+function init(){
+    loadName();
+}
+
+init();
