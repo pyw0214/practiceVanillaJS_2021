eeb8078f3a90d1275666b9081506b4a916430c1d
pyw0214 <yongwookp@gmail.com>
2021-02-13 19:56:21 +0900

    added_weather_error_invalied_0213

---
+++ weather.js
@@ -0,0 +1,53 @@
+const API_KEY = "56ddc2fec82ee79fcbe0c0c1bf048bb5";
+const COORDS = 'coords';
+
+function getWeather(lat, lng){
+    fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lng}&appid=${API_KEY}`
+    ).then(function(response){
+        return response.json()
+    }).then(function(json){
+        console.log(json);
+    })
+}
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj))
+}
+
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
+}
+
+function handleGeoError(){
+    console.log("Cant access geo location");
+}
+
+
+function askforCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if (loadedCoords === null){
+        askforCoords();
+    } else {
+        const parsedCoords = JSON.parse(loadedCoords); 
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
+    }
+}
+
+
+function init() {
+    loadCoords();
+}
+
+init();
